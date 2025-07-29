915248bb122849a6d9733216ab702aeec7cbffa5
luiz.nazario <luiz.nazario@agpr5.com>
2025-07-29 08:34:50 -0300

    refactor: improve code formatting and structure in meals routes

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -2,12 +2,14 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { randomUUID } from "node:crypto";
 import { knex } from "../database";
+import { request } from "node:http";
+import id from "zod/v4/locales/id.cjs";
 
 interface MeslsProps {
-  id: string,
-  name: string,
-  inside_diet: boolean,
-  user_id: string
+  id: string;
+  name: string;
+  inside_diet: boolean;
+  user_id: string;
 }
 
 export async function mealsRoutes(app: FastifyInstance) {
@@ -17,16 +19,28 @@ export async function mealsRoutes(app: FastifyInstance) {
     return { meals };
   });
 
-  app.post("/", async (request, response) => {
+  app.get("/:id", async (request) => {
+    const getMealsParamSchema = z.object({
+      id: z.string().uuid(),
+    });
 
-    const { id, name, inside_diet, user_id} = request.body as MeslsProps
-    const mealsCreate = await knex('meals').insert({
+    const { id } = getMealsParamSchema.parse(request.params);
+
+    const meals = await knex("meals").where("id", id).first();
+
+    return { meals };
+  });
+
+  app.post("/", async (request, response) => {
+    const { id, name, inside_diet, user_id } = request.body as MeslsProps;
+
+    await knex("meals").insert({
       id: randomUUID(),
       name,
       inside_diet,
-      user_id
+      user_id,
     });
 
-    return response.code(200).send("Meal create success")
-  })
+    return response.status(200).send("Meal create success");
+  });
 }
