36206c356d3d7134a6269817dbce46923d8aa906
luiz.nazario <luiz.nazario@agpr5.com>
2025-07-29 10:57:36 -0300

    refactor: enhance meals route with description field and add update functionality

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,9 +1,10 @@
 import { FastifyInstance } from "fastify";
-import { z } from "zod";
+import { string, uuid, z, ZodString } from "zod";
 import { randomUUID } from "node:crypto";
 import { knex } from "../database";
 import { request } from "node:http";
 import id from "zod/v4/locales/id.cjs";
+import { log } from "node:console";
 
 interface MeslsProps {
   id: string;
@@ -31,25 +32,58 @@ export async function mealsRoutes(app: FastifyInstance) {
     return { meals };
   });
 
-  app.post("/", async (request, response) => {
+  app.post("/", async (request, reply) => {
     const createMealsBodySchema = z.object({
       name: z.string(),
+      description: z.string(),
       inside_diet: z.boolean(),
       user_id: z.string(),
     });
 
-    const { name, inside_diet, user_id } = createMealsBodySchema.parse(
-      request.body
-    );
+    const { name, description, inside_diet, user_id } =
+      createMealsBodySchema.parse(request.body);
 
     await knex("meals").insert({
       id: randomUUID(),
       name,
+      description,
       inside_diet,
       user_id,
     });
 
-    return response.status(200).send("Meal create success!");
+    return reply.status(200).send("Meal create success!");
+  });
+
+  app.put("/:id", async (request, reply) => {
+    const { id } = request.params as any;
+
+    const updateMealsBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      inside_diet: z.boolean(),
+      user_id: z.string(),
+    });
+
+    const { name, description, inside_diet, user_id } =
+      updateMealsBodySchema.parse(request.body);
+
+    try {
+      const updatedRows = await knex("meals").where("id", id).update({
+        name: name,
+        description: description,
+        inside_diet: inside_diet,
+        user_id: user_id,
+      });
+
+      if (updatedRows === 0) {
+        return reply.status(404).send("Item não encontrado");
+      }
+
+      reply.send("Meals updated");
+    } catch (error) {
+      app.log.error(error);
+      reply.status(500).send("Erro ao atualizar a refeição");
+    }
   });
 
   app.delete("/:id", async (request, reply) => {
